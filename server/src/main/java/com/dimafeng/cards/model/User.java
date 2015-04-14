59f7c6ccdc4c68052f4ba6caeb3665b7baef0d45
Dmitry Fedosov <dimafeng@gmail.com>
2015-04-13 22:39:20 -0400

    user service

---
+++ server/src/main/java/com/dimafeng/cards/model/User.java
@@ -0,0 +1,56 @@
+package com.dimafeng.cards.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class User
+{
+    @Id
+    private String id;
+
+    @Indexed
+    private String email;
+    private String password;
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    @JsonIgnore
+    public String getPassword()
+    {
+        return password;
+    }
+
+    @JsonProperty("password")
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    @Override
+    public String toString()
+    {
+        return email;
+    }
+}
