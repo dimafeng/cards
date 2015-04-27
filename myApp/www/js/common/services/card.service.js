0edd0eff9d4873bb283fc9b51d0e59326aba5111
Dmitry Fedosov <dimafeng@gmail.com>
2015-04-26 22:25:39 -0400

    symlinks fix

---
+++ myApp/www/js/common/services/card.service.js
@@ -0,0 +1,15 @@
+angular.module('starter').service("CardService", function ($resource, BASE_URL) {
+        return {
+            getResource: function () {
+                return $resource(BASE_URL + 'cards/:id', {id: '@id'}, {
+                        'byPlaylist': {
+                            url: BASE_URL + 'playlists/:playlistId/cards',
+                            method: 'GET',
+                            isArray: true
+                        }
+                    }
+                )
+            }
+        }
+    }
+);
---
+++ myApp/www/js/common/services/user.service.js
@@ -0,0 +1,37 @@
+'use strict';
+
+angular.module('starter').service("UserService", function ($http, BASE_URL, RouteService, $q) {
+    var currentUser = null;
+
+    return {
+        signUp: function (credentials) {
+            $http.post(BASE_URL + 'users/signup', credentials).success(function (res) {
+                console.log(res);
+            });
+        },
+        login: function (credentials) {
+            return $q(function (resolve, reject) {
+                $http.post(BASE_URL + 'login', $.param(credentials), {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    }
+                }).success(function (res) {
+                    currentUser = res;
+                    resolve();
+                    RouteService.goAfterLogin();
+                }).error(function(){
+                    reject();
+                });
+            });
+        },
+        isLoggedIn: function () {
+            return currentUser != null;
+        },
+        checkAuth: function () {
+            $http.get(BASE_URL + 'users').success(function (res) {
+                currentUser = res;
+                RouteService.goAfterLogin();
+            });
+        }
+    };
+});
