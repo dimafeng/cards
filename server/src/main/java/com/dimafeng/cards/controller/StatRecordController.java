132450a04b2174ac78218cd611c0e91449973c4a
Dmitry Fedosov <dimafeng@gmail.com>
2015-06-08 22:23:20 -0400

    statistic aggregation

--- server/src/main/java/com/dimafeng/cards/controller/StatRecordController.java
+++ server/src/main/java/com/dimafeng/cards/controller/StatRecordController.java
@@ -3,16 +3,24 @@ package com.dimafeng.cards.controller;
 import com.dimafeng.cards.model.StatRecord;
 import com.dimafeng.cards.repository.StatRecordRepository;
 import com.dimafeng.cards.service.UserService;
+import com.dimafeng.cards.utils.DateUtils;
+import org.javatuples.Pair;
+import org.javatuples.Triplet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.*;
 
 @Controller
 @RequestMapping("/statRecords")
@@ -38,5 +46,38 @@ public class StatRecordController implements CRUDMapping<StatRecord, StatRecord>
     @Override
     public MongoRepository<StatRecord, String> getRepository() {
         return repository;
+    }
+
+    @RequestMapping("/stat")
+    @ResponseBody
+    public Map<Date, Pair<Integer, Integer>> getStatistic() {
+
+        Map<Date, Pair<Integer, Integer>> result = repository
+                .findByDateAfter(DateUtils.asDate(LocalDate.now().minus(1, ChronoUnit.MONTHS)))
+                .stream()
+                .flatMap(e ->
+                        e.getStatistic().entrySet().stream().map(v ->
+                                        Triplet.with(e.getDate(), v.getKey(), v.getValue())
+                        ))
+                .collect(groupingBy(Triplet::getValue0))
+                .entrySet()
+                .stream()
+                .map(e -> Pair.with(
+                        e.getKey(),
+                        e.getValue()
+                                .stream()
+                                .collect(groupingBy(Triplet::getValue1, summarizingInt(Triplet::getValue2)))
+                                .values()
+                                .stream().reduce(Pair.<Integer, Integer>with(0,0), (p, v) -> {
+                                    if(v.getSum()>0) {
+                                        return Pair.with(p.getValue0() + 1, p.getValue1());
+                                    } else {
+                                        return Pair.with(p.getValue0(), p.getValue1() + 1);
+                                    }
+                                }, (a , b) -> Pair.with(a.getValue0() + b.getValue0(),
+                                a.getValue1() + b.getValue1())))
+                ).collect(toMap(Pair::getValue0, Pair::getValue1));
+
+        return result;
     }
 }
---
+++ server/src/test/java/com/dimafeng/cards/controller/StatRecordControllerTest.java
@@ -0,0 +1,67 @@
+package com.dimafeng.cards.controller;
+
+
+import com.dimafeng.cards.model.StatRecord;
+import com.dimafeng.cards.repository.StatRecordRepository;
+import com.dimafeng.cards.utils.DateUtils;
+import org.javatuples.Pair;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.mockito.Mockito.*;
+
+public class StatRecordControllerTest {
+
+    @Test
+    public void test() {
+        StatRecordController statRecordController = new StatRecordController();
+
+        statRecordController.repository = mock(StatRecordRepository.class);
+        when(statRecordController.repository.findByDateAfter(any())).thenReturn(Arrays.asList(
+                createStatRecord(createDate(1), Arrays.asList(
+                         Pair.with("1", 1),
+                        Pair.with("2", 1),
+                        Pair.with("3", -1),
+                        Pair.with("4", -1)
+                )),
+                createStatRecord(createDate(2), Arrays.asList(
+                        Pair.with("1", 1),
+                        Pair.with("2", -1),
+                        Pair.with("3", -1),
+                        Pair.with("4", -1)
+                )),
+                createStatRecord(createDate(3), Arrays.asList(
+                        Pair.with("1", 1),
+                        Pair.with("2", 1),
+                        Pair.with("3", 1),
+                        Pair.with("4", 1)
+                )),
+                createStatRecord(createDate(1), Arrays.asList(
+                        Pair.with("1", -1),
+                        Pair.with("2", 1),
+                        Pair.with("3", 1),
+                        Pair.with("4", -1)
+                ))
+        ));
+
+        System.out.println(statRecordController.getStatistic());
+    }
+
+    private StatRecord createStatRecord(Date date, List<Pair<String, Integer>> statistic) {
+        StatRecord statRecord = new StatRecord();
+        statRecord.setDate(date);
+        statRecord.setStatistic(statistic.stream().collect(Collectors.toMap(Pair::getValue0, Pair::getValue1)));
+        return statRecord;
+    }
+
+    private Date createDate(int shift) {
+        return DateUtils.asDate(LocalDate.now().minus(shift, ChronoUnit.DAYS));
+    }
+}
